e430d92039a035cf057f88648cfb7ef122cb0dd5
Timothy Shores <timothymshores@gmail.com>
2019-05-14 11:44:55 -0500

    Install react-testing-library, import the render method from RTL into app.test.js and write test that checks if <App /> renders

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+    it('renders fine', () => {
+        render(<App />);
+    });
+})
+
